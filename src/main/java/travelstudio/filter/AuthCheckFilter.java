dbbb25d9b6136767bc2a517273fdaafdc3e8d109
SeungMin Kim <k0102027@naver.com>
2017-09-04 18:19:02 +0900

    필터
    필터

---
+++ src/main/java/travelstudio/filter/AuthCheckFilter.java
@@ -0,0 +1,43 @@
+package travelstudio.filter;
+
+/* 역할: HttpSession 객체에 로그인 회원 정보가 있는 검사한다. */
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import travelstudio.domain.Member;
+
+@WebFilter({"/desktop/article/write.html", "/desktop/article/edit.html"})
+public class AuthCheckFilter implements Filter {
+  
+  @Override
+  public void init(FilterConfig filterConfig) throws ServletException {}
+
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+      throws IOException, ServletException {
+    HttpServletRequest httpRequest = (HttpServletRequest) request;
+    HttpServletResponse httpResponse = (HttpServletResponse) response;
+    
+    Member loginMember = (Member)httpRequest.getSession().getAttribute("loginMember");
+    if (loginMember == null) { 
+      httpResponse.sendRedirect("/desktop/login/login.html");
+      return;
+    }
+    
+    chain.doFilter(request, response);
+  }
+
+  @Override
+  public void destroy() {}
+
+}
