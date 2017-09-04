11220910da07f48aee42a4e4d63c928841534e5c
Kjinseo <kinbb1002@hanmail.net>
2017-09-04 17:13:12 +0900

    터
    데이터

---
+++ SHARE/sql-data/travel.sql
@@ -0,0 +1,28 @@
+/*회원*/
+insert into member (email, pwd, alias, intro, path) values('purpleswan@hanmail.net', password('1111'), '보라백조','여행을 사랑하는 보라 백조입니다.','/image/swan.png' );
+insert into member (email, pwd, alias, intro, path) values('minkdak@hanmail.net', password('1111'), '밍그닥', '밍그밍그밍그닥.','/image/man1.jpg' );
+insert into member (email, pwd, alias, intro, path) values('jinyoung@hanmail.net', password('1111'), '진영짱','오호라 ~ 내가 국내여행의 왕.', '/image/eom.png' );
+insert into member (email, pwd, alias, intro, path) values('emma@hanmail.net', password('1111'), 'JInseo','영국을 사랑하는..', '/image/emma.jpg' );
+insert into member (email, pwd, alias, intro, path) values('yuna@hanmail.net', password('1111'), 'UnaQueen', '빙판 전문 여행가', '/image/yunakim.jpg' );
+insert into member (email, pwd, alias, intro, path) values('bike@hanmail.net', password('1111'), 'Mr.Bike','자전거로 국경을 넘을 수 있따.', '/image/bike.jpg' );
+insert into member (email, pwd, alias, intro, path) values('free@hanmail.net', password('1111'), 'Freedom','자유로운 영혼.', '/image/sam.jpg' );
+
+/*게시물*/
+insert into post(title, sdt, edt, cont, pdt, mno) values('보라 백조의 스페인 여행기', '2016-03-07', '2016-03-27', '/image/hat.jpg', '2016-03-29', '1');
+insert into post(title, sdt, edt, cont, pdt, mno) values('청산에 살어리릿다~~', '2017-04-06', '2016-04-09', '/image/valle.jpg', '2016-04-11', '2');
+insert into post(title, sdt, edt, cont, pdt, mno) values('신사의 나라 영국에서 시작한 유럽 여행', '2015-06-05', '2015-06-27', '/image/london.jpg', '2015-06-29', '3');
+insert into post(title, sdt, edt, cont, pdt, mno) values('곤니찌와 일본 방문기', '2017-01-23', '2016-01-25', '/image/japan.jpg', '2016-01-26', '4');
+insert into post(title, sdt, edt, cont, pdt, mno) values('친구들과 함께한 브리튼 여행', '2016-05-01', '2016-05-27', '/image/sam.jpg', '2016-05-29', '5');
+insert into post(title, sdt, edt, cont, pdt, mno) values('떠나요 둘이서 제주도', '2016-07-01', '2016-07-05', '/image/jeju.jpg', '2016-07-07', '6');
+insert into post(title, sdt, edt, cont, pdt, mno) values('100번째 태백산 정복기', '2017-07-08', '2016-07-8', '/image/taebaek.jpg', '2017-07-9', '7');
+
+insert into post(title, sdt, edt, cont, pdt, mno) values('살아있는 중세를 만나다.', '2015-10-12', '2015-10-27', '/image/hat.jpg', '2016-03-29', '1');
+insert into post(title, sdt, edt, cont, pdt, mno) values('살아있는 중세를 만나다.', '2015-10-12', '2015-10-27', '/image/hat.jpg', '2016-03-29', '1');
+insert into post(title, sdt, edt, cont, pdt, mno) values('살아있는 중세를 만나다.', '2015-10-12', '2015-10-27', '/image/hat.jpg', '2016-03-29', '1');
+insert into post(title, sdt, edt, cont, pdt, mno) values('살아있는 중세를 만나다.', '2015-10-12', '2015-10-27', '/image/hat.jpg', '2016-03-29', '1');
+insert into post(title, sdt, edt, cont, pdt, mno) values('살아있는 중세를 만나다.', '2015-10-12', '2015-10-27', '/image/hat.jpg', '2016-03-29', '1');
+insert into post(title, sdt, edt, cont, pdt, mno) values('살아있는 중세를 만나다.', '2015-10-12', '2015-10-27', '/image/hat.jpg', '2016-03-29', '1');
+insert into post(title, sdt, edt, cont, pdt, mno) values('살아있는 중세를 만나다.', '2015-10-12', '2015-10-27', '/image/hat.jpg', '2016-03-29', '1');
+insert into post(title, sdt, edt, cont, pdt, mno) values('살아있는 중세를 만나다.', '2015-10-12', '2015-10-27', '/image/hat.jpg', '2016-03-29', '1');
+
+/*detail*/
